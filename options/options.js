c2df2415f856bcdc4a8ede44a416d67d9e64610c
Crowdbotics <team@crowdbotics.com>
2021-11-13 12:56:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hierarchy_31980",
+  url: "https://hierarchy_31980.botics.co",
+  api: "https://hierarchy_31980.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
